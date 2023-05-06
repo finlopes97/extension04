d86211a4a23bb2664ed894f1a61aab96f4d2407e
Kyle <kyle.finlayson1997@gmail.com>
2023-05-06 21:32:41 +1000

    what is existence

--- src/extension04.c
+++ src/extension04.c
@@ -140,11 +140,25 @@ void matrix_mul(int16_t *matrix_a, uint8_t *dimensions_of_a, int16_t *matrix_b, 
         return;
     }
 
+    uint8_t manyRows;
+    uint8_t manyCols;
+
+    if (m > p) {
+        manyRows = m;
+    } else {
+        manyRows = p;
+    }
+    if (n > q) {
+        manyCols = n;
+    } else {
+        manyCols = p;
+    }
+
     for (i = 0; i < m; i++) { // For every row of matrix a ->
         for (j = 0; j < q; j++) { // For every column of matrix b ->
-            // result_matrix[i*q+j] = 0; // Clear existing values at address ij of result matrix
-            for (k = 0; k < p; k++) { // For every row in matrix b - >
-                result_matrix[i*q+j] += matrix_a[i*n+k] * matrix_b[k*p+j];
+            result_matrix[i*q+j] = 0; // Clear existing values at address ij of result matrix
+            for (k = 0; k < p; k++) { // For every row in matrix b ->
+                result_matrix[i*q+j] += matrix_a[i*n+k] * matrix_b[k*q+j];
             }
         }
     }
